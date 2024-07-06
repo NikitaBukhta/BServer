c92736b97f7d07f0f7b3554e7ac96730b1743fa5
Mykyta Bukhta <buhtanikita977@gmail.com>
2024-07-06 16:58:01 +0300

    update generate log file

--- Initializer/private/include/LoggerInitializer.hpp
+++ Initializer/private/include/LoggerInitializer.hpp
@@ -1,11 +1,16 @@
 #ifndef __SERVER_INITIALIZER_LOGGERINITIALIZER_HPP__
 #define __SERVER_INITIALIZER_LOGGERINITIALIZER_HPP__
 
+#include <string>
 namespace server::initializer {
 
 class LoggerInitializer {
 public:
     static void init();
+
+private:
+    static std::string current_date_time_as_string(void);
+    static std::string generate_log_file_name(const std::string& log_file_name);
 };
 
 }   // !server::initializer;
--- Initializer/private/src/LoggerInitializer.cpp
+++ Initializer/private/src/LoggerInitializer.cpp
@@ -11,10 +11,11 @@
 namespace server::initializer {
 
 void LoggerInitializer::init() {
+    const std::string LOG_FILE_PATH = "logs/log";
     std::filesystem::create_directory("logs");
     
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/logs.txt", true);
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(generate_log_file_name(LOG_FILE_PATH), true);
 
     console_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^%l%$] [thread %t] [%s][%!] [line %#] %v");
     file_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l] [thread %t] [%s][%!] [line %#] %v");
@@ -27,6 +28,30 @@ void LoggerInitializer::init() {
     spdlog::flush_on(spdlog::level::debug);
     
     SPDLOG_LOGGER_INFO(logger, "Logger initialized");
+}
+
+std::string LoggerInitializer::current_date_time_as_string(void) {
+    // Get the current time using std::chrono
+    auto now = std::chrono::system_clock::now();
+    
+    // Convert the current time to a time_t object
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+    
+    // Convert time_t to a struct tm (broken down time)
+    std::tm tm_struct = *std::localtime(&now_c);
+    
+    // Format the date-time as a string
+    std::stringstream ss;
+    ss << std::put_time(&tm_struct, "%Y.%m.%d_%H.%M.%S");
+    
+    return ss.str();
+}
+
+std::string LoggerInitializer::generate_log_file_name(const std::string& log_file_name) {
+    std::stringstream file_name_stream;
+    file_name_stream << log_file_name << "_" << current_date_time_as_string() << ".log";
+
+    return std::move(file_name_stream.str());
 }
 
 }   // !server::initializer;
